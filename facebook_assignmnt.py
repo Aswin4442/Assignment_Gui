91a5edb12d836af4f1855ce696163f242e53bc11
Aswin4442 <aswinbabu4442@gmail.com>
2024-11-26 13:35:39 +0530

    Add files via upload

---
+++ facebook_assignmnt.py
@@ -0,0 +1,40 @@
+# from tkinter import*
+# from tkinter import ttk
+
+# user=Tk()
+
+# user.title("https://web.facebook.com/login/device/")
+
+# label=Label(user,text="facebook",fg="royalblue3",font=(("Klavika","65","bold")))
+# label.place(x=150,y=10)
+
+# label1=Label(user,text="Mobile number OR email address",bg="antique white",width=40,height=1,fg="gray60",font=("calibri","13"))
+# label1.place(x=150,y=150)
+
+# label2=Label(user,text="Password",bg="antique white",width=40,height=1,fg="gray60",font=("calibri","13"))
+# label2.place(x=150,y=189)
+
+# button=Button(user,text="Log In",bg="blue",width=35,height=1,fg="white",activebackground="seashell2",font=("helvetica","17","bold"))
+# button.place(x=86,y=250)
+
+# label4=Label(user,text="Forgotten password?",fg="royalblue2",font=("segoe ui light","13","bold"))
+# label4.place(x=254,y=315)
+
+# label5=Label(user,text="------------------------------------------   or   ------------------------------------------",fg="gray28",font=("seashell2","13","bold"))
+# label5.place(x=57,y=380)
+
+# button=Button(user,text="Create New Account",bg="green4",width=30,height=1,fg="white",activebackground="dark green",font=("calibri","17","bold"))
+# button.place(x=155,y=460)
+
+# label6=Label(user,text="About      Help      More",fg="gray23",font=("calibri","15"))
+# label6.place(x=238,y=650)
+
+# label7=Label(user,text="Meta @ 2024",fg="gray48",font=("calibri","14"))
+# label7.place(x=286,y=700)
+
+# path=PhotoImage(file="C:/Users/Python/Pictures/02778808-2a37-492b-b301-aaf735e22116-cover.png")
+# facebook=Label(user,image=path,width=450,height=800)
+# facebook.place(x=800,y=50)
+
+# user.geometry("1000x800")
+# user.mainloop()
